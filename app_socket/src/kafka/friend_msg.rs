44ad0c16b18e1a8efeb9d1875436322f1a30d3a9
thorli <tt@gmail.com>
2025-06-27 01:35:25 +0800

    init

--- app_socket/src/kafka/friend_msg.rs
+++ app_socket/src/kafka/friend_msg.rs
@@ -6,30 +6,17 @@ use biz_service::protocol::protocol::{Envelope, EnvelopeType, FriendEventMessage
 use bytes::Buf;
 use bytes::Bytes;
 use common::util::date_util::now;
-use std::sync::Arc;
 use log::{info, warn};
 use prost::Message;
 use rdkafka::message::{Message as KafkaMessageTrait, OwnedMessage};
+use std::sync::Arc;
 
 pub async fn friend_msg_to_socket(mut body: impl Buf, msg: &OwnedMessage, socket_manager: &Arc<SocketManager>) -> Result<()> {
-    let message = FriendEventMessage::decode(&mut body)?;
+    let mut message = FriendEventMessage::decode(&mut body)?;
     let message_id = message.event_id.clone();
-
-    get_pending_acks().insert(
-        message_id.clone(),
-        PendingMeta {
-            topic: msg.topic().to_string(),
-            partition: msg.partition(),
-            offset: msg.offset(),
-        },
-    );
-
-    let envelope = Envelope {
-        envelope_id: message_id.clone(),
-        envelope_type: EnvelopeType::ServerToClient as i32,
-        timestamp: now(),
-        payload: Some(FriendEvent(message.clone())),
-    };
+    message.event_id = "".to_string();
+    get_pending_acks().insert(message_id.clone(), PendingMeta { topic: msg.topic().to_string(), partition: msg.partition(), offset: msg.offset() });
+    let envelope = Envelope { envelope_id: message_id.clone(), envelope_type: EnvelopeType::ServerToClient as i32, timestamp: now(), payload: Some(FriendEvent(message.clone())) };
 
     let mut buf = Vec::with_capacity(envelope.encoded_len());
     envelope.encode(&mut buf)?;
